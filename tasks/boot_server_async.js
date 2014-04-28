85531edba3f910c0640831baf73e6be7814298b8
Michelle Kam <michellekam@gmail.com>
2014-04-27 18:50:43 -0700

    working plugin

--- tasks/boot_server_async.js
+++ tasks/boot_server_async.js
@@ -14,19 +14,22 @@ module.exports = function (grunt) {
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerMultiTask('boot_server_async', 'Boot a server asynchrounously. Task completes on seeing a specific line from the server on stdout.', function () {
+    grunt.registerMultiTask('boot_server_async', 'Boot a server asynchronously. Task completes when a specific line from the server is sent to stdout.', function () {
         // Merge task-specific and/or target-specific options with these defaults.
-        var done = this.async();
+
+        var done = this.async();    //Grunt waits for your asynchronous task to complete.
         var options = this.options({
             cmd:'rails server',
-            //args:'server',
-            //gemset: 'showroom_harmony_cms',
+            args:'',
+            gemset: 'showroom_harmony_cms',
             env:{},
             cwd:'.',
-            failOnError: true
+            failOnError: true,
+            matchString: 'pid='   //default which applies to rails server. (indicates rails server booted successfully)
         });
 
-        // TODO Find way of changing current working directory, plugins shouldn't change the workding directory
+        // TODO: Find a better way to change current working directory, plugins shouldn't change the working directory
+        //http://gruntjs.com/creating-plugins#avoid-changing-the-current-working-directory:-process.cwd
         grunt.file.setBase(options.cwd);
 
         var server_cmd = options.cmd;
@@ -36,15 +39,14 @@ module.exports = function (grunt) {
             env:options.env
         };
 
-
-
-        // TODO Iterate over all process environment variables
+        // TODO: Iterate over all process environment variables, not just GEM_PATH
         process.env['GEM_PATH'] = options.env['GEM_PATH'];
 
         var child_process = exec(server_cmd, options.args, execOptions, function(err, stdout, stdin){
 
         });
 
+        // data from child goes to output
         var captureOutput = function (child, output) {
             child.pipe(output);
         };
@@ -52,7 +54,6 @@ module.exports = function (grunt) {
         grunt.verbose.writeln('Command:', server_cmd);
 
         captureOutput(child_process.stdout, process.stdout);
-
         captureOutput(child_process.stderr, process.stderr);
 
         var printOutput = true;
@@ -62,10 +63,13 @@ module.exports = function (grunt) {
             grunt.verbose.writeln(">>>> CHUNK: "+chunk + " >>> END CHUNK");
 
             if(printOutput){
-                printOutput = false;
-                child_process.stdout.removeListener('data', myChunkFunction);
-                grunt.verbose.writeln('>>>>>>>>>>>>>>>>>>>>>> Killing Piping');
-                done();
+
+                if (chunk.indexOf(options.matchString) !== -1) {
+                    child_process.stdout.removeListener('data', myChunkFunction);
+                    grunt.verbose.writeln('>>>>>>>>>>>>>>>>>>>>>> Killing Piping');
+                    printOutput = false;
+                    done();
+                }
             }
         };
 
